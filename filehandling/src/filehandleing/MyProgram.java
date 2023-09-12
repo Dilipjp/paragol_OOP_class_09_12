e6b56cb285c8db59b1cddc1dc8c5cb3eb3433049
Dilip <jpdilipk@gmail.com>
2023-09-12 10:33:55 -0400

    FileNotFoundException added

--- filehandling/src/filehandleing/MyProgram.java
+++ filehandling/src/filehandleing/MyProgram.java
@@ -7,16 +7,25 @@ import java.util.Scanner;
 
 public class MyProgram {
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args)  {
 		// TODO Auto-generated method stub
 		
-		PrintWriter pw = new PrintWriter(new FileOutputStream("Dilip.txt"));
+		PrintWriter pw = null;
 		String s;
 		Scanner kb = new Scanner(System.in);
+		try {
+			pw = new PrintWriter(new FileOutputStream("Dilip.txt"));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			//e.printStackTrace();
+			System.out.println("File not found");
+		}
+		
 		System.out.println("Please give me yout new name");
 		s = kb.nextLine();
-		pw.println(s);
+		pw.println("Your name is " + s);
 		pw.close();
+		System.out.println("File created thank you.");
 
 	}
 
