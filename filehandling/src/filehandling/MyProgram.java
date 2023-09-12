266e27b6a75c6ec9a8d2ff2452f2543215cdfc5e
Dilip <jpdilipk@gmail.com>
2023-09-12 10:20:58 -0400

    Initial commit

---
+++ filehandling/src/filehandling/MyProgram.java
@@ -0,0 +1,23 @@
+package filehandling;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class MyProgram {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		// TODO Auto-generated method stub
+		
+		PrintWriter pw = new PrintWriter(new FileOutputStream("Dilip.txt"));
+		String s;
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Please give me yout new name");
+		s = kb.nextLine();
+		pw.println(s);
+		pw.close();
+
+	}
+
+}
